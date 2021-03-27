307ce097d7d103d079020e79ca653d05ee865552
Whelan Callahan <whelan@Whelans-MacBook-Pro.local>
2021-03-27 11:34:56 -0500

    first lesson

---
+++ start_here.py
@@ -0,0 +1,40 @@
+# This is a comment. Just start with the '#' and the rest of the line is
+# ignored by python
+
+# This is how you 'define' variables
+this_is_a_number = 12
+this_is_also_a_number = 12.2
+
+# The first one is an integer (whole number). The second one is a floating
+# point numer, called a 'float'. It just means a decimal
+
+# Here are some other types of variables
+this_is_a_bool = True
+this_is_string = 'hello!'
+
+# These are all the basic types in python. We'll can make more complex things
+# soon
+
+# You can print all the basic types like this
+print(this_is_a_number)
+print(this_is_also_a_number)
+print(this_is_a_bool)
+print(this_is_string)
+
+# You can also 'redefine' variables later
+this_is_a_number = 'something else'
+this_is_also_a_number = 'something else'
+this_is_a_bool = 'something else'
+this_is_string = 'something else'
+
+# And they don't need to be the same type. For example, you can reassign a
+# boolean value to be a string
+print('\n--- Now you try ---\n')
+
+# Make a string called 'my_name' and assign it your name
+
+# Make two different variables called 'a' and 'b' and store numbers in them
+
+# Now make a variable called c and assign them 'a + b'
+
+# Print 'my_name' and 'c'
