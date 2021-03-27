936821cab8b9713e9cf8f76e8d8dac27416cf692
whelan <callahanwhelan@gmail.com>
2021-03-27 12:45:30 -0500

    lesson 5 & 6

---
+++ lesson_5.py
@@ -0,0 +1,22 @@
+# Here is a list. It's very useful
+x = [1, 2, 3]
+
+# You can store any type in an array
+x = [1, 'hello', False]
+
+# You can even store an array in an array
+x = ['hello', True, [1, 2, 3]]
+
+# If you want to get a value out you can 'index' it
+print(x[1])
+
+# Usually programming languages start with 0 instead of 1. So if I want to get
+# 'hello', I need to use the 0th index
+print(x[0])
+
+# I can also keep adding to the list
+x.append('a new thing')
+print(x[3])
+
+# And I can always get the length of x
+print(len(x))
---
+++ lesson_6.py
@@ -0,0 +1,31 @@
+# When you have lists, loops are very important
+x = [1, 2, 3, 4, 5]
+
+# This is a for loop
+for value in x:
+    print(value)
+
+# You can read it like, "for each value in x"
+
+# You can also just loop a bunch of times
+for i in range(5):
+    print('this will print 5 times')
+
+# Range just means 0 up to the number you specify. You can do more advanced
+# stuff with it but usually it's for this. Read as "for i in the range of 0-4".
+# It goes "up to" 5 but not actually 5. So it loops 5 times but starting at
+# zero
+
+# There is also a while loop
+x = 0
+while x < 5:
+    print('this will also print 5 times')
+    x += 1
+
+# This does the exact same thing as the for loop above but is a little more
+# to write out. Sometimes, they're more helpful than a for loop depending on
+# the situation.
+
+x = ['another', 'fun', 'array']
+while len(x) > 0:
+    print(x.pop())
